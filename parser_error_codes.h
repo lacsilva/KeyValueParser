7f8f7c29b665f2980895a39c1631e1f22ede6c6c
Luis Silva <Luis.Silva@glasgow.ac.uk>
2015-02-26 17:59:56 +0000

    Copyright and documentation.

--- parser_error_codes.h
+++ parser_error_codes.h
@@ -1,3 +1,4 @@
+! Copyright  L. Silva (lacsilva@gmail.com), 2013-2015
 #define WARN_END_OF_FILE           -100000
 #define ERR_WRONG_ENTRY_IN_SECTION  100000
 #define ERR_INVALID_ENTRY           200000
